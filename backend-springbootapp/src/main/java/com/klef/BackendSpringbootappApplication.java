2a64233627290d2044c82c44f41f3d6f4c5bf4a9
chaitanya <ayekanagachaitanya@gmail.com>
2025-10-18 12:22:25 +0530

    done

--- backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
+++ backend-springbootapp/src/main/java/com/klef/BackendSpringbootappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BackendSpringbootappApplication extends SpringBootServletInitializerializer {
+public class BackendSpringbootappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendSpringbootappApplication.class, args);
